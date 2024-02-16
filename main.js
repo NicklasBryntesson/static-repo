8e415d8d9871d3c821a6a3c35d029b0eba3b9767
Nicklas Bryntesson <nicklas.bryntesson@kan.se>
2024-02-16 14:48:42 +0100

    Import remote designsystem as a npm package!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-import './src/js/script.js';
+import "./src/js/script.js";
 
-import './src/css/style.css';
+import "@nicklas-bryntesson-kan/distributed-designsystem-example/designsystem/designsystem.css";
+
+import "./src/css/style.css";
